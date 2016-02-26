0c37e46652c2598bd07ac097420767141b6a2bda
Dharm Kapadia <dharmkap@gmail.com>
2016-02-26 15:40:40 -0500

    Fix modal popup to use CSS3 only, no JS

--- js/indexcontroller.js
+++ js/indexcontroller.js
@@ -1,81 +1,39 @@
-var myApp = angular.module('myApp', ['ui.bootstrap']);
+var myApp = angular.module('myApp', []);
 
 myApp.config(['$httpProvider', function($httpProvider) {
         $httpProvider.defaults.useXDomain = false;
         $httpProvider.defaults.withCredentials = false;
         delete $httpProvider.defaults.headers.common["X-Requested-With"];
         $httpProvider.defaults.headers.common["Accept"] = "application/json";
-        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";    
+        $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
     }
 ]);
 
-myApp.controller('IndexController', function($scope, $window, $uibModal) {
+myApp.controller('IndexController', function($scope, $window) {
+    $scope.popupView = new popup();
+
     $scope.tasks = [
         {
             userId:1,
             taskId:1,
-            title:'GitHub',
-            completed:'false',
-            url:'https://www.github.com/dharmkap'
-        },
-        {
-            userId:2,
-            taskId:2,
             title:'CNN',
             completed:'false',
             url:'http://www.cnn.com'
         },
         {
-            userId:3,
-            taskId:3,
+            userId:2,
+            taskId:2,
             title:'Ness SES',
             completed:'false',
             url:'http://www.ness-ses.com'
-        },
-        {
-            userId:4,
-            taskId:4,
-            title:'Google News',
-            completed:'false',
-            url:'http://news.google.com'
-        },
+        }
     ];
 
-    $scope.myGridOptions = {
-        showFooter: false,
-        enableSorting: true,
-        multiSelect: false,
-        enableFiltering: true,     
-        enableRowSelection: true, 
-        enableSelectAll: false,
-        enableRowHeaderSelection: false,  
-        enableGridMenu: true,
-        noUnselect: true,
-        onRegisterApi: function (gridApi) {
-            $scope.gridApi = gridApi;
-        },
-
-        rowTemplate: "<div ng-dblclick=\"getExternalScopes().onDblClick(row)\" ng-repeat=\"(colRenderIndex, col) in colContainer.renderedColumns track by col.colDef.name\" class=\"ui-grid-cell\" ng-class=\"{ 'ui-grid-row-header-cell': col.isRowHeader }\" ui-grid-cell ></div>"
-    };
-
     $scope.dblclick = function(task) {
-        const modalTemplate = `
-            <div>
-                <iframe width='800px' height='600px' src="` + task.url + `"></iframe>
-            </div>
-        `;
-
-        const modalInstance = $uibModal.open({
-            animation: true,
-            template: modalTemplate,
-            controller: ModalInstanceController
+        document.getElementById('myIframe').src = task.url;
+        $scope.popupView.show(document.querySelector('#iframe_popup'), undefined ,function() {
+            document.getElementById('myIframe').src = null;
+            console.log("hidden");
         });
-
-        return modalInstance;
-
-        // $window.open(task.url, "_blank", "height=600,width=800,toolbar=no,location=no,menubar=no,titlebar=no");
     };
-
-    function ModalInstanceController($scope, $uibModalInstance) {
-    }
 });
